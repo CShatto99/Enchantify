99bd8dbf93bf2a2afff762a54d8634efd023f8ee
Cameron Shatto <cameronshatto99@gmail.com>
2024-01-19 03:36:27 -0600

    fix(main): change bot status

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { ActivityType, Client, Events, GatewayIntentBits } from 'discord.js';
 import { BotClient } from './@types/custom';
 import config from './utils/config';
 import createInteraction from './utils/createInteraction';
 import importCommands from './utils/importCommands';
 import registerSlashCommands from './utils/registerSlashCommands';
 
-export const client = new Client({
+export const client: BotClient = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildMessageReactions,
     GatewayIntentBits.MessageContent,
   ],
-}) as BotClient;
+});
 
 client.once(Events.ClientReady, () => {
+  client.user.setActivity({
+    name: 'Managing Enchantments📖',
+    type: ActivityType.Custom,
+  });
   importCommands();
   createInteraction();
   registerSlashCommands();
