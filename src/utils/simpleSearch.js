1a62108f8cf08eb3fbca0b025de2810d7176ac05
Cameron Shatto <cameronshatto99@gmail.com>
2024-01-19 22:57:16 -0600

    fix(main): remove .js files

--- src/utils/simpleSearch.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function simpleSearch(query, list) {
-    const regex = new RegExp(query, 'i'); // 'i' flag makes the search case-insensitive
-    return list.filter(item => regex.test(item));
-}
-exports.default = simpleSearch;
